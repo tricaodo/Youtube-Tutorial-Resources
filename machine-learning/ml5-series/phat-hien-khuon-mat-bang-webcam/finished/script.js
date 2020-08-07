c0849142f412fafc2c02c6a22c6697059023907b
Tri Do <trido1903@gmail.com>
2020-08-07 10:35:01 -0700

    added layout for hangman game

--- machine-learning/ml5-series/phat-hien-khuon-mat-bang-webcam/finished/script.js
+++ machine-learning/ml5-series/phat-hien-khuon-mat-bang-webcam/finished/script.js
@@ -6,6 +6,7 @@ function setup() {
   createCanvas(640, 480);
   // 2. Tạo video webcam.
   video = createCapture(VIDEO, () => console.log("Video is ready."));
+  video.hide();
   // 3. Truy cập thư viện library.
   faceApi = ml5.faceApi(video, modelLoaded);
 }
@@ -21,11 +22,11 @@ function response(error, results) {
     return;
   }
   // Check nếu array is empty. Nếu không sẽ bị lỗi khi truy cập index 0.
-  if (results.length > 0) {
+  for (let result of results) {
     // Vẽ video lên canvas.
     image(video, 0, 0, width, height);
     // Vẽ cái box xung quanh khuôn mặt.
-    drawBox(results[0].alignedRect);
+    drawBox(result.alignedRect);
   }
   // Gọi lại function này để cập nhật khuôn mặt.
   faceApi.detect(video, response);
