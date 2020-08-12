6523b048c2f8b1877ee1bde2002a8e56968164cf
Tri Do <trido1903@gmail.com>
2020-08-12 10:58:22 -0700

    rm bingAPi

--- machine-learning/idol-search-app/src/apis/bingApi.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://idolsearchdemo.cognitiveservices.azure.com/bing/v7.0/images/",
-    headers: { "Ocp-Apim-Subscription-Key": "a1c0c9824ef6404796249ed86a7d6371" }
-})
